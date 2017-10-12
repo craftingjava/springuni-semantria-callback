3fdf2ddda0ec4ed857b1cd1088f16572cbc080c7
Laszlo Csontos <github@laszlocsontos.com>
2017-10-12 09:12:02 +0200

    Initial commit

---
+++ src/main/java/com/springuni/springunisemantriacallback/SpringuniSemantriaCallbackApplication.java
@@ -0,0 +1,12 @@
+package com.springuni.springunisemantriacallback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringuniSemantriaCallbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringuniSemantriaCallbackApplication.class, args);
+	}
+}
